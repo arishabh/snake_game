e8a9be99e3cd2a67e5b2684b2ef1a5d1cc80cc06
rishabhagrawal41 <rishabhagrawal41>
2019-12-23 20:11:34 -0500

    Done with all

--- snake_game.py
+++ snake_game.py
@@ -1,102 +1,138 @@
 import pygame
-from pygame import draw
+import random
+import time
 
 # Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
- 
-def draw_board(screen):
-    # Head
-    draw.rect(screen, BLACK, (0,0,30,30))
- 
-    # # Legs
-    # pygame.draw.line(screen, BLACK, [5 + x, 17 + y], [10 + x, 27 + y], 2)
-    # pygame.draw.line(screen, BLACK, [5 + x, 17 + y], [x, 27 + y], 2)
-    #
-    # # Body
-    # pygame.draw.line(screen, RED, [5 + x, 17 + y], [5 + x, 7 + y], 2)
-    #
-    # # Arms
-    # pygame.draw.line(screen, RED, [5 + x, 7 + y], [9 + x, 17 + y], 2)
-    # pygame.draw.line(screen, RED, [5 + x, 7 + y], [1 + x, 17 + y], 2)
- 
-# Setup
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+snake_size = 20
+food_size = 10
+
 pygame.init()
- 
-# Set the width and height of the screen [width,height]
-size = [700, 700]
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-# Loop until the user clicks the close button.
+
 quit = False
- 
-# Used to manage how fast the screen updates
+paused = False
+size = [800, 600]
+
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Snake Game")
+
+clock_spd = 5
+snake_len = 1
+snake_list = []
+x_coord = size[0]/2
+y_coord = size[1]/2
+x_vel = 0
+y_vel = 0
+food_x = random.randint(20, size[0]-20)
+food_y = random.randint(20, size[1]-20)
+score = 0
+fuel = 0
+
+vel = snake_size
+boost_vel = 2*snake_size
+
+
 clock = pygame.time.Clock()
- 
-# Hide the mouse cursor
-pygame.mouse.set_visible(0)
- 
-# Speed in pixels per frame
-x_speed = 0
-y_speed = 0
- 
-# Current position
-x_coord = 10
-y_coord = 10
- 
-# -------- Main Program Loop -----------
+
+def message(msg, color):
+    font = pygame.font.SysFont(None, 100)
+    mes = font.render(msg, True, color)
+    screen.blit(mes, [size[0]/2-200, size[1]/2])
+
+def draw_snake(li):
+    for x in li[:-1]:
+        pygame.draw.rect(screen, red, [x[0], x[1], snake_size, snake_size])
+    snake = pygame.draw.rect(screen, black, [li[-1][0], li[-1][1], snake_size, snake_size])
+    return snake
+
+def pause_mes():
+    font = pygame.font.SysFont(None, 60)
+    mes = font.render("Paused", True, red)
+    screen.blit(mes, [size[0]/2-100, 20])
+    font = pygame.font.SysFont(None, 40)
+    mes = font.render("p - Pause", True, red)
+    screen.blit(mes, [size[0]/2-70, 80])
+    mes = font.render("SPACE - Boost", True, red)
+    screen.blit(mes, [size[0]/2-70, 120])
+    mes = font.render("q - Quit", True, red)
+    screen.blit(mes, [size[0]/2-70, 200])
+
 while not quit:
-    # --- Event Processing
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit = True
-            # User pressed down on a key
- 
-        elif event.type == pygame.KEYDOWN:
-            # Figure out if it was an arrow key. If so
-            # adjust speed.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                paused = not paused
+                if paused:
+                    temp_x = x_vel
+                    temp_y = y_vel
+                    x_vel = 0
+                    y_vel = 0
+                else:
+                    x_vel = temp_x
+                    y_vel = temp_y
+
+            if paused: continue
             if event.key == pygame.K_LEFT:
-                x_speed = -3
-            elif event.key == pygame.K_RIGHT:
-                x_speed = 3
-            elif event.key == pygame.K_UP:
-                y_speed = -3
-            elif event.key == pygame.K_DOWN:
-                y_speed = 3
- 
-        # User let up on a key
-        elif event.type == pygame.KEYUP:
-            # If it is an arrow key, reset vector back to zero
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_speed = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_speed = 0
- 
-    # --- Game Logic
- 
-    # Move the object according to the speed vector.
-    x_coord = x_coord + x_speed
-    y_coord = y_coord + y_speed
- 
-    # --- Drawing Code
- 
-    # First, clear the screen to WHITE. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-    screen.fill(WHITE)
- 
-    draw_board(screen)
- 
- 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # Limit frames per second
-    clock.tick(60)
- 
-# Close the window and quit.
+                if x_vel == 0: 
+                    x_vel = -1*vel
+                    y_vel = 0
+            if event.key == pygame.K_RIGHT:
+                if x_vel == 0: 
+                    x_vel = vel
+                    y_vel = 0
+            if event.key == pygame.K_UP:
+                if y_vel == 0: 
+                    y_vel = -1*vel
+                    x_vel = 0
+            if event.key == pygame.K_DOWN:
+                if y_vel == 0: 
+                    y_vel = vel
+                    x_vel = 0
+            if event.key == pygame.K_q:
+                quit = True
+            if event.key == pygame.K_SPACE:
+                if fuel > 0:
+                    fuel -= 1
+                    clock_spd *= 1.2-fuel
+    
+    x_coord = (x_coord+x_vel)%size[0]
+    y_coord = (y_coord+y_vel)%size[1]
+    head = [x_coord, y_coord]
+    snake_list.append(head)
+    if len(snake_list)>snake_len: snake_list = snake_list[1:]
+    quit = quit or any(map( lambda x: x==head, snake_list[:-1]))
+    
+    screen.fill(white)
+    font = pygame.font.SysFont(None, 30)
+    mes = font.render("Fuel", True, black)
+    screen.blit(mes, [size[0]-50, 20])
+    pygame.draw.rect(screen, green, [size[0]-20, size[1]-40-(fuel*(size[1]-100)), 15, (fuel*(size[1]-100))])
+    food = pygame.draw.rect(screen, green, [food_x, food_y, food_size, food_size])
+    snake = draw_snake(snake_list)
+    font = pygame.font.SysFont(None, 30)
+    mes = font.render("You Score: " + str(score), True, black)
+    screen.blit(mes, [10,10])
+    if paused: pause_mes()
+
+    if(snake.colliderect(food)):
+        score += 10
+        snake_len += 1
+        fuel = min(1, fuel+0.1)
+        clock_spd += (score/40)
+        food_x = random.randint(20, size[0]-20)
+        food_y = random.randint(20, size[1]-20)
+
+    pygame.display.update()
+    clock.tick(clock_spd)
+
+message("Game over", red)
+pygame.display.update()
+time.sleep(2)
+
 pygame.quit()
+exit()
